4d9764d7910898261fdb8c3b8381031e5b5ddc9a
William Millet <z5692883@ad.unsw.edu.au>
2025-09-11 13:19:11 +1000

    Remove test accidentally placed in __init__ file

--- src/order_book/__init__.py
+++
@@ -1,8 +0,0 @@
-from order_book.order_book import OrderBook
-
-def test_book_is_initially_empty():
-    book = OrderBook()
-
-    assert book.best_bid() is None
-    assert book.best_offer is None
-    
